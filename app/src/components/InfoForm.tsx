b16b399ebb727bc8f9fdc4c3b8d0adc711facf17
thegolbehead <x.movie.effects.x@gmail.com>
2022-04-09 12:22:11 +0300

    added procfile and gunicorn

--- app/src/components/InfoForm.tsx
+++ app/src/components/InfoForm.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import '../styles/info-form.css';
 
 function InfoForm() {
-  return (
-    <div className="info-form">
-        <label htmlFor="full-url">Full url:</label>
-        <input type="text" id="full-url"/>
-        <label htmlFor="short-url">Shortened url:</label>
-        <div className="bottom-form">
-            <input type="text" id="short-url"/>
-            <div className="buttons">
-                <button type="button" className="btn btn-warning">Clear</button>
-                <button type="button" className="btn btn-secondary">Shorten</button>
+    return (
+        <div className="info-form">
+            <label htmlFor="full-url">Full url:</label>
+            <input type="text" id="full-url"/>
+            <label htmlFor="short-url">Shortened url:</label>
+            <div className="bottom-form">
+                <input type="text" id="short-url"/>
+                <div className="buttons">
+                    <button type="button" className="btn btn-warning">Clear</button>
+                    <button type="button" className="btn btn-secondary">Shorten</button>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default InfoForm;
