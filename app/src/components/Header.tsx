cb1a288488366db66f4312eb2baa0075e4f40c21
thegolbehead <x.movie.effects.x@gmail.com>
2022-04-09 11:22:43 +0300

    added header component

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="https://github.com/theglobehead/Shortify">Github</a>
+    </div>
+  );
+}
+
+export default Header;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
+import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
